ed8ba658844de42b116c398fc7c925f0d24765f6
YishanSUN <sandrine.sun.yishan@gmail.com>
2022-06-02 00:35:31 +0200

    Add 3 data class and musee object

--- app/src/main/java/com/example/beaux_arts/donnees/Collection.kt
+++ app/src/main/java/com/example/beaux_arts/donnees/Collection.kt
@@ -2,7 +2,7 @@ package com.example.beaux_arts.donnees
 
 data class Collection(
 
-    val numero: Int = 0,
+    val id: Int = 0,
     val nom: String = "null",
     val auteur: String = "anonyme",
     val type: String = "Tableau",
@@ -14,7 +14,7 @@ data class Collection(
 
 ){
     override fun toString(): String {
-        return "Collection(numero=$numero, nom='$nom', auteur='$auteur', type='$type', desciption='$desciption', salle=$salle, mesproduits=$mesproduits, position=${position.contentToString()}, image='$image')"
+        return "Collection(id=$id, nom='$nom', auteur='$auteur', type='$type', desciption='$desciption', salle=$salle, mesproduits=$mesproduits, position=${position.contentToString()}, image='$image')"
     }
 
 }
